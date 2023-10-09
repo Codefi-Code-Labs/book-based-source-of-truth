62eb276024d2a263e00941941db2ff7970849734
WilderDev <williamjohnwilder@outlook.com>
2023-10-09 08:10:43 -0500

    FEATURE: Navbar Basics

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent {
-
+  collapsed: boolean = true;
+  show: boolean = false;
 }
